5dde89bc48e8e71a8b9ee4ee0079bf4d18e4695c
hieuquang2212 <hieu.ngo@igap.vn>
2021-03-14 00:50:11 +0700

    add push() at the front method of linked list class

--- LinkedList.py
+++ LinkedList.py
@@ -13,6 +13,11 @@ class LinkedList:
             print (temp.data)
             temp = temp.next
 
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
 if __name__ == '__main__':
     llist = LinkedList()
     llist.head = Node(1)
